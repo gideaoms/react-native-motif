df92376a62a67589a8f4b2556f55260267003e3e
Gideão <gideaoms@gmail.com>
2023-02-09 12:40:27 -0300

    beta version and readme

---
+++ readme.md
@@ -0,0 +1,57 @@
+# Motif (beta)
+
+This is the initial version of Motif, a React Native Library to style components.
+
+src/motif.tsx
+
+```tsx
+import { createTheme } from '@/motif'
+
+export const { ThemeProvider, useTheme, theme, styled } = createTheme({
+  colors: {
+    primary: 'red',
+    secondary: 'blue',
+  },
+  fontSizes: {
+    sm: 15,
+    md: 18,
+  },
+  spaces: {
+    sm: 5,
+    md: 10,
+  },
+})
+```
+
+src/components/box.tsx
+
+```tsx
+import { View } from 'react-native'
+import { styled } from '@/styled'
+
+export const Box = styled(View, theme => ({
+  width: '100%',
+  variants: {
+    padding: {
+      md: {
+        padding: theme.spaces.md,
+      },
+      sm: {
+        padding: theme.spaces.sm,
+      },
+    },
+  },
+}))
+```
+
+src/app.tsx
+
+```
+export function App() {
+  return (
+    <Box padding="md">
+      <Text>Hello world</Text>
+    </Box>
+  )
+}
+```
---
+++ src/mod.tsx
@@ -0,0 +1,84 @@
+import {
+  ComponentProps,
+  ComponentType,
+  createContext,
+  createElement,
+  forwardRef,
+  ReactNode,
+  useContext,
+} from 'react'
+import { ImageStyle, TextStyle, ViewStyle } from 'react-native'
+
+type BaseStyle = ViewStyle & TextStyle & ImageStyle
+
+type Variants = { [key: string]: { [key: string]: BaseStyle } }
+
+export function createTheme<Theme>(theme: Theme) {
+  const Context = createContext<Theme>(theme)
+
+  function useTheme() {
+    return useContext(Context)
+  }
+
+  function ThemeProvider(props: { children: ReactNode; theme: Theme }) {
+    return (
+      <Context.Provider value={props.theme}>{props.children}</Context.Provider>
+    )
+  }
+
+  function createStyledVariants(
+    props: ComponentProps<any>,
+    variants?: Variants,
+  ) {
+    if (!variants || typeof variants !== 'object') {
+      return {}
+    }
+    return Object.keys(props).reduce((prev, key) => {
+      const pairs = variants[key]
+      if (!pairs) {
+        return prev
+      }
+      const variant = props[key]
+      if (!variant) {
+        return prev
+      }
+      const pair = pairs[variant]
+      if (!pair) {
+        return prev
+      }
+      return { ...prev, ...pair }
+    }, {})
+  }
+
+  function styled<
+    Component extends ComponentType<any>,
+    Style extends BaseStyle & { variants?: Variants },
+    Props = Component extends ComponentType<infer Props> ? Props : never,
+  >(component: Component, createStyledTheme: (theme: Theme) => Style) {
+    const styledTheme = createStyledTheme(theme)
+    return forwardRef<
+      Component,
+      Props & {
+        style?: BaseStyle
+      } & {
+        [K in keyof Style['variants']]?: keyof Style['variants'][K]
+      }
+    >((props, ref) => {
+      const styledVariants = createStyledVariants(props, styledTheme.variants)
+      const styledInline = props.style
+      const style = { ...styledTheme, ...styledVariants, ...styledInline }
+      return createElement(component, {
+        ...props,
+        ref,
+        style,
+      })
+    })
+  }
+
+  return {
+    styled,
+    theme,
+    useTheme,
+    ThemeProvider,
+  }
+}

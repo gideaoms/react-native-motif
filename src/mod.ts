91c0568745f4ebe09f5a8e53409dcdff7c176642
Gideão <gideaoms@gmail.com>
2024-08-16 09:44:16 -0300

    new styled

--- src/mod.ts
+++ src/mod.ts
@@ -2,35 +2,37 @@ import { TextStyle, ViewStyle, ImageStyle } from 'react-native'
 
 type Style = TextStyle & ViewStyle & ImageStyle
 
-type GetFn<T, K extends keyof T> = {
-  readonly get: <U extends keyof T[K]>(key: (keyof T[K] extends 'true' | 'false' ? boolean : keyof T[K]) | undefined) => T[K][U] | undefined
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+type GET<T, K extends keyof T, U extends keyof T[K]> = {
+  readonly get: (
+    variant: (U extends 'true' | 'false' ? boolean : U) | undefined
+  ) => T[K][U] | undefined
 }
 
 export type VariantProps<T> = {
-  [K in keyof T]?: keyof T[K] extends 'true' | 'false' | 'get' ? boolean : Exclude<keyof T[K], 'get'>
+  [K in keyof T]?:
+  keyof T[K] extends 'true' | 'false' ? boolean : Exclude<keyof T[K], 'get'>
 }
 
 export function styled<
-  const T extends {
-    [K in keyof Style]: {
-      [Y in string]: Style[K]
-    }
-  },
+  const T extends { [V in keyof Style]: { [Y in string]: Style[V] } },
   K extends keyof T,
   U extends keyof T[K]
 >(config: T) {
-  const mapped: any = {}
-  for (const key in config) {
-    mapped[key] = {
+  const keys = Object.keys(config) as K[]
+  return keys.reduce((acc, key) => ({
+    ...acc,
+    [key]: {
       ...config[key],
-      get: (variant: U | undefined) => {
+      get: function (variant: U | undefined) {
         if (variant === undefined) {
           return undefined
         }
-        return config[key as unknown as K][variant]
-      }
-    }
-  }
-  return mapped as { [U in K]: T[U] & GetFn<T, U> }
+        return config[key][variant]
+      },
+    },
+  }), {} as Prettify<Record<K, Prettify<T[K] & GET<T, K, U>>>>)
 }
-
